2aec155bcfd1f5ca5f8eb9be8e275c4add6c5c62
Abdelrhman Ashrf <abdelrhmanashrf1997@gmail.com>
2024-06-21 23:53:32 +0300

    User authentication task completed

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->redirectTo(
+            guests:'account/login',
+            users:'account/profile',
+        );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('account/register', [AccountController::class,'register'] )->name('account.register');
-Route::post('account/register', [AccountController::class,'processRegister'] )->name('account.processRegister');
 
-Route::get('account/login', [AccountController::class,'login'] )->name('account.login');
+
+
+
+Route::group(['prefix'=> 'account'], function () {
+    Route::group(['middleware'=>'guest'], function(){
+
+        Route::get('register', [AccountController::class,'register'] )->name('account.register');
+        Route::post('register', [AccountController::class,'processRegister'] )->name('account.processRegister');
+
+        Route::get('login', [AccountController::class,'login'] )->name('account.login');
+        Route::post('login', [AccountController::class,'authenticate'] )->name('account.authenticate');
+
+    });
+    Route::group(['middleware'=>'auth'], function(){
+        Route::get('profile', [AccountController::class,'profile'] )->name('account.profile');
+        Route::get('logout', [AccountController::class,'logout'] )->name('account.logout');
+    });
+});
 
 Route::middleware([
     'auth:sanctum',
