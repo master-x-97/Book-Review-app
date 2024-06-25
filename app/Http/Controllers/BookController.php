ec2d489fb2d12404b4671786cfb417247e4c848b
Abdelrhman Ashrf <abdelrhmanashrf1997@gmail.com>
2024-06-25 12:47:48 +0300

    Edit Book task

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File as FacadesFile;
 use Illuminate\Support\Facades\Validator;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
+use Illuminate\Support\Facades\File;
 
 class BookController extends Controller
 {
@@ -72,11 +74,63 @@ class BookController extends Controller
         return redirect()->route('books.index')->with('success','Book added successfully');
     }
     //this method will show edit book page 
-    public function edit(){
+    public function edit($id){
+        $book = Book::findOrFail($id);
+        // dd($book);
+        return view('books.edit',[
+            'book'=> $book
+        ]);
 
     }
     //this method will update book 
-    public function update(){
+    public function update($id , Request $request){
+        $book = Book::findOrFail($id);
+        $rules =  [
+            'title'=>'required|min:5',
+            'author'=>'required|min:3',
+            'status'=>'required',
+        ];
+        if(!empty($request->image)){
+            $rules['image'] = 'image';
+        }
+
+        $validator = Validator::make($request->all(),$rules );
+        if ($validator->fails()) {
+            return redirect()->route('books.edit',$book->id)->withErrors($validator)->withInput();
+        }
+
+        //Update book in  DB
+        $book->title = $request->title;
+        $book->description = $request->description;
+        $book->author = $request->author;
+        $book->status = $request->status;
+        $book->save();
+
+        // upload book image here
+        if(!empty($request->image)){
+
+            //this will delete old book image from books directory
+
+        File::delete(public_path('uploads/profile/'.$book->image));
+        File::delete(public_path('uploads/profile/thumb/'.$book->image));
+
+            $image = $request->image;
+            $ext = $image->getClientOriginalExtension() ;
+            $imageName = time().'.'.$ext;
+            $image->move( public_path('uploads/books'), $imageName);
+
+            $book->image = $imageName;
+            $book->save();
+
+
+            // generate image thumbnail here
+            $manager = new ImageManager(Driver::class);
+            $img = $manager->read(public_path('uploads/books/'.$imageName)); 
+            $img->resize(990); 
+            $img->save(public_path('uploads/books/thumb/'.$imageName));
+        }
+
+        return redirect()->route('books.index')->with('success','Book updated successfully');
 
     }
     //this method will delete a book from page
